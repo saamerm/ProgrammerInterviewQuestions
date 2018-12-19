6e0d90c8ee7065ecaaf0c72821bc1b9d0526aa01
Saamer Mansoor <i@saamer.me>
2018-12-18 22:55:51 -0500

    Initial Setup
    Put in instructions containing questions and the architecture to be followed

---
+++ AimForAmazon/GroupA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace AimForAmazon
+{
+	public class GroupA
+	{
+		public GroupA()
+		{
+			Question1();
+		}
+
+		void Question1()
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ AimForAmazon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AimForAmazon
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var x = new GroupA();
+		}
+	}
+}
