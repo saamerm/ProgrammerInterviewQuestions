491623915d3865a71a277eb6697f17c87609bec4
Saamer Mansoor <i@saamer.me>
2018-12-19 13:23:42 -0500

    More work for Q1

--- AimForAmazon/GroupA.cs
+++ AimForAmazon/GroupA.cs
@@ -8,9 +8,16 @@ namespace AimForAmazon
 			Question1();
 		}
 
+		// 1. How do you find the missing number in a given integer array of 1 to 100? (solution)
 		void Question1()
 		{
+			var x = new int[]{ 6, 3, 4, 5, 1 };
 			Console.WriteLine("Hello World!");
+			// Create a sorted array with the input first
+			for (var i = 0; i < x.Length; i++)
+			{
+				if (x[i+1]. )
+			}
 		}
 	}
 }
