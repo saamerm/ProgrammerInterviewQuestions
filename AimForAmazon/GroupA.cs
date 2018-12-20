c17ac79ff5b7fa66424e1e7ddad7acfaab77dd75
Saamer Mansoor <i@saamer.me>
2018-12-19 23:00:17 -0500

    Completed Question1

--- AimForAmazon/GroupA.cs
+++ AimForAmazon/GroupA.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace AimForAmazon
 {
 	public class GroupA
@@ -6,18 +8,40 @@ namespace AimForAmazon
 		public GroupA()
 		{
 			Question1();
+			Question1UsingStatics();
 		}
-
+		#region Quesiton1
 		// 1. How do you find the missing number in a given integer array of 1 to 100? (solution)
 		void Question1()
 		{
-			var x = new int[]{ 6, 3, 4, 5, 1 };
+			var x = new int[] { 6, 3, 14, 5, 1 };
 			Console.WriteLine("Hello World!");
-			// Create a sorted array with the input first
-			for (var i = 0; i < x.Length; i++)
-			{
-				if (x[i+1]. )
-			}
+			var h = new HelperFunctions();
+			h.ArrayPrinter(x);
+			h.ArrayAscendingSorter(x);
+			// Since arrays are passed by value, 
+			// the array doesn't need to be passed around
+			h.ArrayPrinter(x);
+			h.FindMissingNumberFromSortedArray(x);
 		}
+
+		void Question1UsingStatics()
+		{
+			var x = new int[] { 6, 3, 14, 5, 1 };
+			Console.WriteLine("Hello World!");
+			StaticHelperFunctions.ArrayPrinter(x);
+			StaticHelperFunctions.ArrayAscendingSorter(x);
+			StaticHelperFunctions.ArrayPrinter(x);
+			StaticHelperFunctions.FindMissingNumberFromSortedArray(x);
+		}
+		#endregion Quesiton1
+
+		#region Quesiton2
+		// 2. How do you find the duplicate number on a given integer array? (solution)
+		void Question2()
+		{
+			var x = new int[] { 6, 3, 14, 5, 1 };
+		}
+		#endregion Quesiton2
 	}
 }
---
+++ AimForAmazon/StaticHelperFunctions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AimForAmazon
+{
+	public static class StaticHelperFunctions
+	{
+		public static void FindMissingNumberFromSortedArray(int[] arr)
+		{
+			var missingNumbersCount = 0;
+			var list = new List<int>();
+
+			// Tells us only if the next number is missing
+			//for (var i = 0; i < arr.Length -1; i ++)
+			//{
+			//	if (arr[i + 1] - arr[i] != 1)
+			//	{
+			//		list.Add(arr[i] + 1);
+			//		missingNumbersCount++;
+			//	}
+			//}
+
+			for (var i = 0; i < arr.Length - 1; i++)
+			{
+				var x = arr[i + 1] - arr[i];
+				var y = arr[i];
+				while (x != 1)
+				{
+					// y++ adds the wrong number to the list
+					// we want to add the next number to the list (y+1) 
+					list.Add(++y);
+					missingNumbersCount++;
+					x--;
+				}
+			}
+
+			if (missingNumbersCount > 0)
+			{
+				Console.WriteLine(missingNumbersCount + " Missing Number(s):");
+				foreach (var item in list)
+					Console.Write(item + " ");
+			}
+			else
+				Console.WriteLine("No missing numbers found");
+		}
+
+		public static void ArrayAscendingSorter(int[] arr)
+		{
+			int y;
+			for (var j = 0; j < arr.Length - 1; j++)
+			{
+				// Get the largest value to the end
+				for (var i = 0; i < arr.Length - 1 - j; i++)
+				{
+					if (arr[i + 1] < arr[i])
+					{
+						y = arr[i];
+						arr[i] = arr[i + 1];
+						arr[i + 1] = y;
+					}
+
+				}
+			}
+		}
+		public static void ArrayPrinter(int[] arr)
+		{
+			Console.Write("{ ");
+			for (var i = 0; i < arr.Length; i++)
+				Console.Write(arr[i] + " ");
+			Console.WriteLine("}");
+		}
+	}
+}
