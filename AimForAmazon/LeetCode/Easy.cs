c36fdd9a95638c05baeadf32cc67ca3dd6ca193d
Saamer Mansoor <i@saamer.me>
2019-01-02 20:32:52 -0500

    Starting Question2 Leetcode

--- AimForAmazon/LeetCode/Easy.cs
+++ AimForAmazon/LeetCode/Easy.cs
@@ -11,8 +11,11 @@ namespace AimForAmazon.LeetCode
 			var arr1 = new int[] { 2, 7, 11, 15 };
 			var arr2 = new int[] { 3, 2, 4 };
 			Question1(arr2);
+			Question2(arr2);
 		}
 
+		#region Question1
+		// https://leetcode.com/problems/two-sum/
 		// Called TwoSum
 		private void Question1(int[] arr)
 		{
@@ -22,6 +25,8 @@ namespace AimForAmazon.LeetCode
 			h.ArrayPrinter(arr2);
 		}
 		// Only faster than 16% of submissions
+		// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+		// You may assume that each input would have exactly one solution, and you may not use the same element twice.
 		private int[] TwoSum_TimeComplex(int[] nums, int target)
 		{
 			for (int i = 0; i < nums.Length; i++)
@@ -39,7 +44,8 @@ namespace AimForAmazon.LeetCode
 			}
 			return new int[0];
 		}
-		public int[] twoSum(int[] nums, int target)
+		//Try to do better
+		private int[] TwoSum_TimeBetter(int[] nums, int target)
 		{
 			var map = new Dictionary<int, int>();
 			for (int i = 0; i < nums.Length; i++)
@@ -53,5 +59,33 @@ namespace AimForAmazon.LeetCode
 			}
 			throw new Exception("No two sum solution");
 		}
+		#endregion Question1
+		#region Question2
+		// Called AddTwoNumbers
+		// https://leetcode.com/problems/add-two-numbers/
+		private void Question2(int[] arr)
+		{
+			var h = new HelperFunctions();
+			var target = 6;
+			var arr2 = TwoSum_TimeComplex(arr, target);
+			h.ArrayPrinter(arr2);
+		}
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int x) { val = x; }
+		}
+		//You are given two non-empty linked lists representing two non-negative integers.The digits are stored in reverse order and each of their nodes contain a single digit.Add the two numbers and return it as a linked list.
+		//You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+		//Example:
+		//Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+		//Output: 7 -> 0 -> 8
+		//Explanation: 342 + 465 = 807.
+		public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+		{
+			return l1;
+		}
+		#endregion Question2
 	}
 }
